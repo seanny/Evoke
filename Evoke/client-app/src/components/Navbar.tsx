0b83e7ff7878f7283d368faf4da589aeda09e0b5
Sean McElholm <seanmcelholm@gmail.com>
2023-06-29 14:03:49 +0100

    Navbar completed

--- Evoke/client-app/src/components/Navbar.tsx
+++ Evoke/client-app/src/components/Navbar.tsx
@@ -1,59 +1,113 @@
 import * as React from 'react';
 import './Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import logo from "../logo192.png";
+
+export const isHiddenContext = React.createContext(true);
+
+export interface ItemProps {
+    name: string;
+    isActive: boolean;
+    href: string;
+}
+
+function Item({ name, isActive, href }: ItemProps) {
+    let classes = "nav-link";
+    if (isActive) {
+        classes += " active";
+    }
+
+    return (
+        <li className="nav-item">
+            <a className={classes} href={href}>{name}</a>
+        </li>
+    )
+}
+
+export interface ButtonProps {
+    href: string;
+    children: any;
+}
+
+function ItemButton({ href, children }: ButtonProps) {
+    let classes = "item button";
+
+    return (
+        <li className={classes}><a href={href}>{children}</a></li>
+    )
+}
+
+export interface SubMenuProps {
+    name: string;
+    children: any;
+}
+
+function SubMenu({ name, children }: SubMenuProps) {
+    return (
+        <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">{name}</a>
+            <ul className="dropdown-menu">
+                {children}
+            </ul>
+        </li>
+    )
+}
+
+export interface SubitemProps {
+    name: string;
+    href: string;
+}
+
+function Subitem({ name, href }: SubitemProps) {
+    let classes = "dropdown-item";
+
+    return (
+        <li className={classes}><a href={href}>{name}</a></li>
+    )
+}
 
 export default function Navbar() {
-    var isHidden: boolean = true;
-    const toggle = document.querySelector(".toggle");
-    const menu = document.querySelector(".menu");
-    const items = document.querySelectorAll(".item");
+    const [isHidden, setHidden] = React.useState(true);
 
     const ToggleMenu = () => {
-        if (menu?.classList.contains("active")) {
-            menu.classList.remove("active");
-
-            if (toggle != null) {
-                var a = toggle?.querySelector("a");
-                if (a != null) {
-                    a.innerHTML = "O";
-                }
-            }
-        }
-        else {
-            menu?.classList.add("active");
-
-            // adds the close (x) icon 
-            if (toggle != null) {
-                var a = toggle?.querySelector("a");
-                if (a != null) {
-                    a.innerHTML = "C";
-                }
-            }
-        }
+        setHidden(!isHidden);
     };
 
     return (
-        <nav className="navbar">
-            <ul className="menu">
-                <li className="logo"><a href="#">Evoke</a></li>
-                <li className="toggle"><a href="#" onClick={ToggleMenu}>TOG</a></li>
-                <li className="item"><a href="#">Sale</a></li>
-                <li className="item has-submenu">
-                    <a tabIndex={ 0 }>All Clothing</a>
-                    <ul className="submenu">
-                        <li className="subitem"><a href="#">View All</a></li>
-                        <li className="subitem"><a href="#">Tops</a></li>
-                        <li className="subitem"><a href="#">Hoodies</a></li>
-                        <li className="subitem"><a href="#">Jeans</a></li>
-                        <li className="subitem"><a href="#">Joggers</a></li>
-                        <li className="subitem"><a href="#">Leggings</a></li>
-                        <li className="subitem"><a href="#">Shorts</a></li>
+        <nav className="navbar navbar-expand-lg evoke-nav-bg">
+            <div className="container-fluid">
+                <div className="float-start">
+                    <button className="evoke-navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                        <FontAwesomeIcon icon={faBars} />
+                    </button>
+                    <a className="navbar-brand" href="#"><img height="30" src={logo} /></a>
+                </div>
+                <div className="collapse navbar-collapse" id="navbarCollapse">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <Item name="Sale" isActive={false} href="#"/>
+                        <SubMenu name="All Clothing">
+                            <Subitem name="View All" href="#" />
+                            <Subitem name="Tops" href="#" />
+                            <Subitem name="Hoodies" href="#" />
+                            <Subitem name="Jeans" href="#" />
+                            <Subitem name="Joggers" href="#" />
+                            <Subitem name="Leggings" href="#" />
+                            <Subitem name="Shorts" href="#" />
+                        </SubMenu>
+                        <Item name="Tops" isActive={false} href="#" />
                     </ul>
-                </li>
-                <li className="item"><a href="#">Tops</a></li>
-                <li className="item button"><a href="#">Account</a></li>
-                <li className="item button"><a href="#">Favourites</a></li>
-                <li className="item button"><a href="#">Shopping Cart</a></li>
-            </ul>
+                </div>
+                <div className="d-flex">
+                    <a className="navbar-brand" href="#"><FontAwesomeIcon icon={faUser} /></a>
+                    <a className="navbar-brand" href="#"><FontAwesomeIcon icon={faHeart} /></a>
+                    <a className="navbar-brand" href="#"><FontAwesomeIcon icon={faCartShopping} /></a>
+                </div>
+            </div>
         </nav>
     )
 }
